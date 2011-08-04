f41a4263901b9b07e1dc74e43f4347e1390d50eb
Steve <steve.hook@gmail.com>
2011-08-04 20:19:41 +0100

    Failing test for part 3 - should prompt for another input

--- kata1_ruby/kernel_extensions.rb
+++ kata1_ruby/kernel_extensions.rb
@@ -1,10 +1,14 @@
 module Kernel
   class << self
-    attr_accessor :last_output
+    attr_accessor :last_output, :next_input
   end
 
   def puts(output)
     Kernel.last_output ||= []
     Kernel.last_output << output.to_s
+  end
+
+  def gets
+    Kernel.next_input
   end
 end
--- kata1_ruby/scalc_spec.rb
+++ kata1_ruby/scalc_spec.rb
@@ -12,7 +12,12 @@ describe "Calculator command-line" do
   it 'should print the result' do
     ARGV[0] = '1,2,3'
     load './scalc.rb'
-    Kernel.last_output.length.should == 1
+    Kernel.last_output.length.should == 2
     Kernel.last_output[0].should == 'The result is 6'
+    Kernel.last_output[1].should == 'another input please'
+  end
+
+  it "should exit when an empty string is given" do
+    pending
   end
 end
