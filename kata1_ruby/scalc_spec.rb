346b757fd1beb5201af02ffe75f7db4eafc88647
Steve <steve.hook@gmail.com>
2011-08-04 19:40:21 +0100

    Added failing test for scalc

--- kata1_ruby/scalc_spec.rb
+++ kata1_ruby/scalc_spec.rb
@@ -1,5 +1,25 @@
+module Kernel
+  class << self
+    attr_accessor :last_output
+  end
+
+  def puts(output)
+    Kernel.last_output = output.to_s
+  end
+end
+
 describe "Calculator command-line" do
+  before(:each) do
+    Kernel.last_output = nil
+  end
+
   it 'should run' do
-    require './scalc'
+    load './scalc.rb'
+  end
+
+  it 'should print the result' do
+    ARGV = ['1,2,3']
+    load './scalc.rb'
+    Kernel.last_output.should == 'The result is 6'
   end
 end
