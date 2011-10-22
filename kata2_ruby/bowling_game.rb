2916803ef54e9c68a974bc9257d39e2da5ea62d4
Steve Hook <steve.hook@gmail.com>
2011-10-22 11:34:16 +0100

    Fixed example for spare

--- kata2_ruby/bowling_game.rb
+++ kata2_ruby/bowling_game.rb
@@ -1,13 +1,26 @@
 class BowlingGame
   def initialize
     @total = 0
+    @frames = []
   end
 
-  def roll(pins)
-    @total += pins
+  def roll(score)
+    last = @frames.last
+    if last && last.size < 2
+      last << score
+    else
+      @frames << [score]
+    end
   end
 
   def score
-    @total
+    total_score = 0
+    is_spare = false
+    @frames.each do |frame|
+      frame_score = frame.inject { |sum, n| sum + n }
+      total_score += is_spare ? frame[0] * 2 + frame[1] : frame_score
+      is_spare = frame_score == 10
+    end
+    total_score
   end
 end
