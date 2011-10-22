0cc446db62189b04b7f1bcb39b347e727411f1db
Steve Hook <steve.hook@gmail.com>
2011-10-22 10:57:40 +0100

    Failing example for all 1s

--- kata2_ruby/bowling_game_spec.rb
+++ kata2_ruby/bowling_game_spec.rb
@@ -2,14 +2,17 @@ require 'rspec'
 require './bowling_game'
 
 describe BowlingGame do
-  context "Gutter game" do
-    before(:each) do
-      @game = BowlingGame.new
-    end
+  before(:each) do
+    @game = BowlingGame.new
+  end
 
-    it "score should be zero" do
-      20.times { @game.roll(0) }
-      @game.score.should == 0
-    end
+  it "score should be 0 for gutter game" do
+    20.times { @game.roll(0) }
+    @game.score.should == 0
+  end
+
+  it "should score 20 when all ones are thrown" do
+    20.times { @game.roll(1) }
+    @game.score.should == 20
   end
 end
