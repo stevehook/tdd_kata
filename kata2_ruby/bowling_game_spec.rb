a6be974d7f210ce55b6c29203d742dfe010e8fe5
Steve Hook <steve.hook@gmail.com>
2011-10-22 10:54:29 +0100

    Failing example for gutter game score

--- kata2_ruby/bowling_game_spec.rb
+++ kata2_ruby/bowling_game_spec.rb
@@ -2,5 +2,14 @@ require 'rspec'
 require './bowling_game'
 
 describe BowlingGame do
+  context "Gutter game" do
+    before(:each) do
+      @game = BowlingGame.new
+    end
 
+    it "score should be zero" do
+      20.times { @game.roll(0) }
+      @game.score.should == 0
+    end
+  end
 end
