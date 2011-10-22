0d66192439ef51a3a86b7c3971ab3ba113a2edb8
Steve Hook <steve.hook@gmail.com>
2011-10-22 11:38:21 +0100

    Failing example for throwing a strike

--- kata2_ruby/bowling_game_spec.rb
+++ kata2_ruby/bowling_game_spec.rb
@@ -16,11 +16,19 @@ describe BowlingGame do
     @game.score.should == 20
   end
 
-  it "should double next score when a spare is thrown" do
+  it "should double next score of next when a spare is thrown" do
     @game.roll(5)
     @game.roll(5)
     @game.roll(3)
     17.times { @game.roll(0) }
     @game.score.should == 16
+  end
+
+  it "should double score of next two throws when a strike is thrown" do
+    @game.roll(10)
+    @game.roll(5)
+    @game.roll(3)
+    16.times { @game.roll(0) }
+    @game.score.should == 26
   end
 end
